b831fbdfda6ceab5e8147b58f664f86cf7e17d39
unknown <pinoignacio@uniovi.es>
2023-12-10 17:16:40 +0100

    routerBids and GET endpoints

---
+++ data/bids.js
@@ -0,0 +1,4 @@
+let bids = [
+    {id:1 , userId:1, itemId: 1, amount:200 }
+]
+module.exports = bids
---
+++ routers/routerBids.js
@@ -0,0 +1,17 @@
+let express = require("express")
+let routerBids = express.Router();
+let bids = require("../data/bids")
+
+routerBids.get("/", (req, res) => {
+    res.json(bids)
+})
+
+routerBids.get("/:id", (req, res) => {
+    let currentBid = bids.find( bid => bid.id == req.params.id)
+    if ( currentBid == undefined){
+        res.status(400).json({ error: "not bid with that id "})
+        return
+    }
+    res.json(currentBid)
+})
+module.exports = routerBids
